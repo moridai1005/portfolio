b458bff0f3518e6797b5e5c82baeba949a936fd4
moridai1005 <49890145+moridai1005@users.noreply.github.com>
2019-12-12 11:16:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ $(function () {
   $('.each').hover(
     function() {
       $(this).find('.eachbox').fadeIn();
+      $(this).find('.acc').html('詳細を非表示<a><span class="fas fa-arrow-up"></span></a>');
+      $(this).find('.acc').addClass("dummy");
     },
     function() {
       $(this).find('.eachbox').fadeOut();
@@ -29,5 +31,27 @@ $(function () {
       var hre = $(this).find('a').attr('href');
       $("html,body").animate({scrollTop:$(hre).offset().top},700);
     }
+  );
+
+  $('.toolbar .fa-bars').click( 
+    function() {
+      if ($('.hid').is(':visible')) {
+      $('.hid').fadeOut(10);
+      $('.toolbar').removeClass('actual');
+    } else {
+      $('.toolbar').addClass('actual');
+      $('.hid').fadeIn();
+    }}
   )
+
+  $('.main').on('click', function () {
+    if ($('.hid').is(':visible')) {
+      $('.hid').fadeOut(200);
+      $('.toolbar').removeClass('actual',3000);
+    } else {
+      event.stopPropagation();
+    }
+  });
+
+
 });
